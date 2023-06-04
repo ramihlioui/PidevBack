566c9ca541d20d604ee1913a19f6070efbe9419c
ramihlioui <ramihlioui2@gmail.com>
2023-06-04 10:28:07 +0200

    started Pidev backend

---
+++ src/main/java/com/example/pidevback/PidevBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.pidevback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PidevBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PidevBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pidevback/PidevBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pidevback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PidevBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
