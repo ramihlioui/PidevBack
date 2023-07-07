f54534f2435708c8c86f6c52b6fc1068188d3d9a
Ameni-BenJeddou <amanibenjeddou.abj@gmail.com>
2023-07-07 16:50:06 +0100

    reclamation service functions

--- src/main/java/com/example/pidevback/services/ReclamationService.java
+++ src/main/java/com/example/pidevback/services/ReclamationService.java
@@ -1,6 +1,8 @@
 package com.example.pidevback.services;
 
 
+import com.example.pidevback.entities.Reclamation;
+import com.example.pidevback.entities.enums.State;
 import com.example.pidevback.repositories.ReclamationRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,4 +13,27 @@ import org.springframework.stereotype.Service;
 public class ReclamationService {
     @Autowired
     private ReclamationRepository reclamationRepository;
+
+    public Reclamation getReclamation(Long id) {
+        return reclamationRepository.findById(id).orElseThrow(RuntimeException::new);
+    }
+
+    public Reclamation saveReclamation (Reclamation reclamation){
+        return reclamationRepository.save(reclamation);
+    }
+
+    public Reclamation ProcessReclamation (Reclamation reclamation){
+        Reclamation rec = reclamationRepository.findById(reclamation.getId()).orElseThrow(RuntimeException::new);
+        rec.setState(State.PROCESSING);
+        return reclamationRepository.save(rec);
+    }
+
+    public Reclamation closeReclamation (Reclamation reclamation,String solution){
+        Reclamation rec = reclamationRepository.findById(reclamation.getId()).orElseThrow(RuntimeException::new);
+        rec.setState(State.CLOSED);
+        rec.setSolution(solution);
+        return reclamationRepository.save(rec);
+    }
+
+
 }
