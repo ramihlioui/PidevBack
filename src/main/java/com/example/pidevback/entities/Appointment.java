9eb6ae65e4cf98d58dace17d1cd92e2601b3f6bb
Ameni-BenJeddou <amanibenjeddou.abj@gmail.com>
2023-07-21 14:06:17 +0100

    final touches

--- src/main/java/com/example/pidevback/entities/Appointment.java
+++ src/main/java/com/example/pidevback/entities/Appointment.java
@@ -1,5 +1,6 @@
 package com.example.pidevback.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
@@ -17,7 +18,10 @@ public class Appointment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate dateFin;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+
     private LocalDate dateDebut;
     @JsonIgnore
     @ManyToOne
